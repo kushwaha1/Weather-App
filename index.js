b64190379b9b4d728e6f698b0ff821a6434b7df4
Sweety <rsweet375@gmail.com>
2025-08-10 15:24:30 +0530

    update api_key and api_url in js file for openweathermap

---
+++ index.js
@@ -0,0 +1,5 @@
+const API_KEY = "36c5af3959c8c8994d4c378f8e135c8f";
+const API_URL = "https://api.openweathermap.org/data/2.5/weather";
+
+console.log("API Key:", API_KEY);
+console.log("API URL:", API_URL);
